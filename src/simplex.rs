1023d5fa117e77062ab2808c74f239124d3c334d
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2019-06-18 03:43:40 +0200

    Fix simplex algorithm for surplus variables
    	Up until now, simplex algorithm couldn't deal with surplus
    	variables due to method differences between phases 1 and 2.
    	Simplex algorithm now passes the improper five_not_uniform test.
    	Still needs some additions for numerical stability.

--- src/simplex.rs
+++ src/simplex.rs
@@ -16,31 +16,72 @@ fn make_a(constraints: &Matrix) -> Matrix {
     a
 }
 
-fn feasible_basic_vector(ab: &MatrixSlice, b: &Vector) -> Vector {
-    let lu = ab.clone().lu();
-    let xb = lu.solve(b).unwrap();
-    if xb.iter().all(|x| *x >= 0f64) {
-        xb
+fn make_indices(b: &Vector, m: usize, n: usize) -> Vec<Option<usize>> {
+    repeat(None).take(m).chain((0..n).map(|i| Some(i))).collect()
+}
+
+fn choose_pivot(a: &Matrix, c: &Vector) -> Option<usize> {
+    let (m, n) = a.shape();
+    println!("a = {}", a);
+    println!("c = {}", c.transpose());
+    println!("(m, n) = ({}, {})", m, n);
+    // Compute reduced costs for all xk not in basis
+    let y = a.columns(0, m).transpose().lu().solve(&c.rows(0, m)).unwrap();
+    println!("y = {}", y.transpose());
+    let u = c.rows(m, n - m) - a.columns(m, n - m).clone().transpose() * y;
+    println!("u = {}", u.transpose());
+    if u.iter().all(|x| *x >= 0f64) {
+        None
     } else {
-        // FIXME: implement actual phase 1 from NR
-        if b.iter().all(|x| *x >= 0f64) {
-            Vector::from_element(xb.len(), 1f64 / (3f64 * xb.len() as f64))
-        } else if b.iter().all(|x| *x <= 0f64) {
-            Vector::from_element(xb.len(), 1f64 / (xb.len() as f64))
-        } else {
-            panic!("Vector doesn't have a constant sign: {}", b.transpose());
-        }
+        Some(u.into_iter().enumerate()
+                          .min_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap())
+                          .unwrap().0 + m)
     }
 }
 
-fn make_indices(b: &Vector, m: usize, n: usize) -> Vec<Option<usize>> {
-    if b.iter().all(|x| *x >= 0f64) {
-        repeat(None).take(m).chain((0..n).map(|i| Some(i))).collect()
-    } else if b.iter().all(|x| *x <= 0f64) {
-        (0..n).map(|i| Some(i)).chain(repeat(None).take(m)).collect()
-    } else {
-        panic!("Vector doesn't have a constant sign: {}", b.transpose());
+fn make_auxiliary_objective(xb: &Vector, l: usize) -> Vector {
+    Vector::from_iterator(l, xb.iter().map(|x|
+        if *x < 0f64 { -1f64 } else { 0f64 }
+    ).chain(repeat(0f64).take(l - xb.len())))
+}
+
+fn feasible_basic_vector(a: &mut Matrix, b: &Vector, c: &mut Vector, ind: &mut Vec<Option<usize>>) -> Vector {
+    let (m, n) = a.shape();
+    let mut xb = Vector::from_element(m, 1f64 / m as f64);
+    xb = a.clone().columns(0, m).lu().solve(b).unwrap();
+    let mut v = make_auxiliary_objective(&xb, c.len());
+    if v.iter().all(|x| *x == 0f64) { return xb; }
+    for _pass in 1..10 {
+        println!("Pass {}", _pass);
+        let lu = a.clone().columns(0, m).lu();
+        xb = lu.solve(b).unwrap();
+        if xb.iter().all(|x| *x >= 0f64) { break; }
+        println!("Feasible basic xb = {}", xb.transpose());
+        //let v = make_auxiliary_objective(&xb, c.len());
+        //if v.iter().all(|x| *x == 0f64) { break; }
+        let k = choose_pivot(a, &v).unwrap();
+        println!("k = {}", k);
+        let ablu = a.columns(0, m).clone().lu();
+        let w = ablu.solve(&a.column(k)).unwrap();
+        println!("w = {}", w.transpose());
+        let ratio = Vector::from_iterator(w.len(), w.iter().zip(xb.iter()).map(|(w, x)| x / w));
+        println!("x / w = {}", ratio.transpose());
+        if let Some((i, _)) = w.into_iter().zip(xb.iter()).enumerate()
+                               .min_by(|(_, (wi, xi)), (_, (wj, xj))|
+                                       (*xi / *wi).partial_cmp(&(*xj / *wj))
+                                                  .unwrap()) {
+            // 5. Swap columns i and k
+            println!("Swapping columns k = {} and i = {}", k, i);
+            a.swap_columns(i, k);
+            c.swap_rows(i, k);
+            v.swap_rows(i, k);
+            ind.swap(i, k);
+        } else {
+            break;
+        }
     }
+    println!("Finished phase 1 with xb = {}", xb.transpose());
+    xb
 }
 
 // NOTE: prints are for debugging purposes and should be removed eventually
@@ -48,9 +89,9 @@ pub fn simplex(constraints: &Matrix, cost: &Vector, b: &Vector) -> Vector {
     let (m, n) = constraints.shape();
     let mut a = make_a(constraints);
     // 1. Find a feasible basis
-    let mut xb = feasible_basic_vector(&a.columns(0, m).clone(), b);
     let mut c = cost.clone();
     let mut ind = make_indices(b, m, n);
+    let mut xb = feasible_basic_vector(&mut a, b, &mut c, &mut ind);
     //println!("b = {}", b.transpose());
     for _pass in 1.. {
         //println!("========== Pass {} ==========", _pass);
@@ -60,15 +101,14 @@ pub fn simplex(constraints: &Matrix, cost: &Vector, b: &Vector) -> Vector {
         //println!("ind = {:?}", ind);
         // 2. Compute reduced costs for all xk not in basis
         //println!("[[[ Phase 2 ]]]");
-        let y = a.columns(0, m).transpose().lu().solve(&c.rows(0, m)).unwrap();
-        let u = c.rows(m, n) - a.columns(m, n).clone().transpose() * y;
         //println!("u = {}", u.transpose());
         // 3. If all uk >= 0, break; otherwise choose most negative uk
         //println!("[[[ Phase 3 ]]]");
-        if u.iter().all(|x| *x >= 0f64) { break; }
-        let k = u.into_iter().enumerate()
-                 .min_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap())
-                 .unwrap().0 + m;
+        let k: usize;
+        match choose_pivot(&a, &c) {
+            None => break,
+            Some(kk) => k = kk,
+        };
         //println!("k = {}", k);
         // 4. Minimum ratio test
         //println!("[[[ Phase 4 ]]]");
@@ -89,10 +129,13 @@ pub fn simplex(constraints: &Matrix, cost: &Vector, b: &Vector) -> Vector {
             c.swap_rows(i, k);
             ind.swap(i, k);
         } else {
-            break;
+            panic!("Fucked up!!! {}", w);
         }
     }
+    println!("Almost finished phase 2 with xb = {}", xb.transpose());
     xb = a.columns(0, m).clone().lu().solve(b).unwrap();
+    println!("Almost finished phase 2 with xb = {}", xb.transpose());
+    println!("with indices {:?}", ind);
     let x = Vector::from_iterator(
         n,
         (0..n).map(|i|
@@ -104,6 +147,7 @@ pub fn simplex(constraints: &Matrix, cost: &Vector, b: &Vector) -> Vector {
             }
         )
     );
+    println!("Finished phase 2 with x = {}", x.transpose());
     x
 }
 
