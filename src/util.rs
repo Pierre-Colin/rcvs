26d6474826e1e5730c3f212fd145d355a4a465b0
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2019-07-15 19:15:19 +0200

    Fix sorting algorithms

--- src/util.rs
+++ src/util.rs
@@ -5,19 +5,27 @@
  * guaranteed to be supported in the future. They should only be used inside
  * the crate and in integration tests.
  */
+use std::cmp::Ordering;
+
 fn insertion_sort<A, F>(a: &mut [A], b: usize, e: usize, compare: &F)
-    where F: Fn(&A, &A) -> std::cmp::Ordering
+    where F: Fn(&A, &A) -> Ordering
 {
     for i in (b + 1)..e {
-        let mut j = i;
-        while j > 0 && compare(&a[j], &a[j - 1]) == std::cmp::Ordering::Less {
+        for j in (1..=i).rev() {
+            if compare(&a[j], &a[j - 1]) != Ordering::Less { break; }
             a.swap(j - 1, j);
         }
+        /*
+        let mut j = i;
+        while j > 0 && compare(&a[j], &a[j - 1]) == Ordering::Less {
+            a.swap(j - 1, j);
+        }
+        */
     }
 }
 
 pub fn quick_sort<A, F>(mut a: Vec<A>, compare: F) -> Vec<A> 
-    where F: Fn(&A, &A) -> std::cmp::Ordering
+    where F: Fn(&A, &A) -> Ordering
 {
     let mut stack = vec![(0usize, a.len())];
     while !stack.is_empty() {
@@ -25,27 +33,27 @@ pub fn quick_sort<A, F>(mut a: Vec<A>, compare: F) -> Vec<A>
         if size <= 7 {
             insertion_sort(&mut a, b, b + size, &compare);
         } else {
-            a.swap(rand::random::<usize>() % size, b);
+            a.swap(b + rand::random::<usize>() % size, b);
             let mut i = b;
-            let mut j = i;
+            let mut j = i + 1;
             let mut k = b + size - 1;
             // Invariant: [i, j) only contains copies of the pivot
-            while j < k {
+            while j <= k {
                 match compare(&a[j], &a[i]) {
-                    std::cmp::Ordering::Less => {
+                    Ordering::Less => {
                         a.swap(i, j);
                         i += 1;
                         j += 1;
                     },
-                    std::cmp::Ordering::Greater => {
+                    Ordering::Greater => {
                         a.swap(j, k);
                         k -= 1;
                     },
-                    std::cmp::Ordering::Equal => j += 1,
+                    Ordering::Equal => j += 1,
                 }
             }
-            stack.push((b, i - b));
-            stack.push((j, size - j));
+            if i > b { stack.push((b, i - b))};
+            if j < b + size { stack.push((j, b + size - j)) };
         }
     }
     a
@@ -58,4 +66,40 @@ pub fn quick_sort<A, F>(mut a: Vec<A>, compare: F) -> Vec<A>
 #[macro_export]
 macro_rules! string_vec {
     ($($x: expr), *) => (vec![$($x.to_string()), *]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn is_nondecreasing<A, F>(a: &[A], compare: F) -> bool
+        where F: Fn(&A, &A) -> Ordering
+    {
+        if !a.is_empty() {
+            let mut last = &a[0];
+            for x in a.iter().skip(1) {
+                if compare(x, last) == Ordering::Less { return false; }
+                last = x;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn quick_sort_test() {
+        for n in 0..=20 {
+            let num_tries = if n < 2 { 1 } else { 10000 };
+            for _ in 0..num_tries {
+                let a: Vec<f64> = (0..n).map(|_| 
+                    rand::random::<f64>() - 0.5f64
+                ).collect();
+                let s = quick_sort(a, |x, y| x.partial_cmp(&y).unwrap());
+                assert!(
+                    is_nondecreasing(&s, |x, y| x.partial_cmp(&y).unwrap()),
+                    "{:?} isn't sorted",
+                    s
+                );
+            }
+        }
+    }
 }
